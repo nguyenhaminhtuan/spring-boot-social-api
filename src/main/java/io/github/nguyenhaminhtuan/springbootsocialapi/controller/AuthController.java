877563387158f0a278669688a707d7bba026f505
Nguyen Ha Minh Tuan <nguyenhaminhtuan1997@gmail.com>
2021-12-09 19:43:02 +0700

    refactor: replace registration, authentication by auth

--- src/main/java/io/github/nguyenhaminhtuan/springbootsocialapi/controller/AuthController.java
+++ src/main/java/io/github/nguyenhaminhtuan/springbootsocialapi/controller/AuthController.java
@@ -1,8 +1,10 @@
 package io.github.nguyenhaminhtuan.springbootsocialapi.controller;
 
+import io.github.nguyenhaminhtuan.springbootsocialapi.dto.request.LoginUserRequest;
 import io.github.nguyenhaminhtuan.springbootsocialapi.dto.request.RegisterUserRequest;
+import io.github.nguyenhaminhtuan.springbootsocialapi.dto.response.LoginResponse;
 import io.github.nguyenhaminhtuan.springbootsocialapi.dto.response.UserResponse;
-import io.github.nguyenhaminhtuan.springbootsocialapi.service.RegistrationService;
+import io.github.nguyenhaminhtuan.springbootsocialapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +17,19 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-public class RegistrationController {
+public class AuthController {
 
-    private final RegistrationService registrationService;
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginUserRequest request) {
+        return ResponseEntity.ok(userService.loginUser(request));
+    }
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<UserResponse> registerUser(@Valid @RequestBody RegisterUserRequest request) {
-        UserResponse response = registrationService.registerUser(request);
+        UserResponse response = userService.registerUser(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/main/java/io/github/nguyenhaminhtuan/springbootsocialapi/service/UserService.java
+++ src/main/java/io/github/nguyenhaminhtuan/springbootsocialapi/service/UserService.java
@@ -1,43 +1,83 @@
 package io.github.nguyenhaminhtuan.springbootsocialapi.service;
 
+import io.github.nguyenhaminhtuan.springbootsocialapi.dto.request.LoginUserRequest;
+import io.github.nguyenhaminhtuan.springbootsocialapi.dto.request.RegisterUserRequest;
 import io.github.nguyenhaminhtuan.springbootsocialapi.dto.request.UpdateUserRequest;
+import io.github.nguyenhaminhtuan.springbootsocialapi.dto.response.LoginResponse;
 import io.github.nguyenhaminhtuan.springbootsocialapi.dto.response.UserResponse;
 import io.github.nguyenhaminhtuan.springbootsocialapi.exception.ResourceNotFoundException;
+import io.github.nguyenhaminhtuan.springbootsocialapi.exception.UsernameAlreadyExistsException;
 import io.github.nguyenhaminhtuan.springbootsocialapi.mapper.UserMapper;
 import io.github.nguyenhaminhtuan.springbootsocialapi.model.User;
+import io.github.nguyenhaminhtuan.springbootsocialapi.model.UserRole;
 import io.github.nguyenhaminhtuan.springbootsocialapi.repository.UserRepository;
+import io.github.nguyenhaminhtuan.springbootsocialapi.security.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper mapper;
+    private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
 
-    public Optional<UserResponse> getUserById(Long id) {
-        Optional<User> user = userRepository.findById(id);
-        if (user.isEmpty()) {
-            return Optional.empty();
+    public LoginResponse loginUser(LoginUserRequest request) {
+        log.debug("Logging in user with credentials {}", request);
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword());
+        Authentication authentication = this.authenticationManager.authenticate(token);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+        log.info("Username {} with authorities [{}] login at {}",
+                principal.getUsername(), principal.getAuthorities(), LocalDateTime.now());
+        return new LoginResponse(authentication.isAuthenticated(), principal.getUsername());
+    }
+
+    public UserResponse registerUser(RegisterUserRequest request) throws UsernameAlreadyExistsException {
+        log.debug("Registering user {}", request);
+        String username = request.getUsername();
+        if (userRepository.existsByUsername(username)) {
+            throw new UsernameAlreadyExistsException(String.format("Username %s already exists", username));
         }
 
-        return Optional.of(mapper.toUserResponse(user.get()));
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setRole(UserRole.USER);
+        userRepository.save(user);
+        log.info("Username {} with role {} registered", user.getUsername(), user.getRole());
+
+        return mapper.toUserResponse(user);
+    }
+
+    public UserResponse getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Not found user with id %d", id)));
+        return mapper.toUserResponse(user);
     }
 
     @PreAuthorize("#id == authentication.principal.id")
     public void updateUser(Long id, UpdateUserRequest request) {
-        Optional<User> userOpt = userRepository.findById(id);
-        if (userOpt.isEmpty()) {
-            throw new ResourceNotFoundException(String.format("Not found user with id %d", id));
-        }
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Not found user with id %d", id)));
 
-        User user = userOpt.get();
         user.setFirstName(request.getFirstName());
         user.setLastName(request.getLastName());
         user.setGender(request.getGender());
